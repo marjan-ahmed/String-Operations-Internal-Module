c42c08a9108ecff1c5d1496a41975ad3f922ef3d
marjan-ahmed <marjan.ahmed08@yahoo.com>
2024-07-19 18:29:46 +0500

    import module from internal module/stringOperations.ts

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var stringOperations_1 = require("./internal module/stringOperations");
-function main() {
-    console.log((0, stringOperations_1.concatenate)("Hello", "World"));
-    console.log((0, stringOperations_1.toUpperCase)("marjan"));
-    console.log((0, stringOperations_1.toLowerCase)("MARJAN"));
-    console.log((0, stringOperations_1.reverseString)("marjan"));
-}
-exports.default = main;
+console.log((0, stringOperations_1.concatenate)("Hello", "World"));
+console.log((0, stringOperations_1.toUpperCase)("marjan"));
+console.log((0, stringOperations_1.toLowerCase)("MARJAN"));
+console.log((0, stringOperations_1.reverseString)("marjan"));
--- main.ts
+++ main.ts
@@ -1,11 +1,6 @@
 import { concatenate, toUpperCase, toLowerCase, reverseString } from "./internal module/stringOperations";
 
-function main() {
-    console.log(concatenate("Hello", "World"));
-    console.log(toUpperCase("marjan"));
-    console.log(toLowerCase("MARJAN"));
-    console.log(reverseString("marjan"));
-}
-
-
-export default main;
+console.log(concatenate("Hello", "World"));
+console.log(toUpperCase("marjan"));
+console.log(toLowerCase("MARJAN"));
+console.log(reverseString("marjan"));
